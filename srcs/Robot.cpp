a76ae1def546b3a7f21887e3bee86915aa9229ea
Marc B <marc.benzahra@gmail.com>
2016-11-12 19:00:18 +0100

    changed action values in Robot.cpp

--- srcs/Robot.cpp
+++ srcs/Robot.cpp
@@ -16,16 +16,16 @@ Robot::Movement::Movement(const simxChar *bodyPart, const std::string &descripti
 /***** Robot *****/
 
 const Robot::movement_list_t Robot::_movements = {
-  { "LeftWheelJoint",  "Move left wheel forward",    20 },
-  { "LeftWheelJoint",  "Move left wheel backward",  -20 },
-  { "RightWheelJoint", "Move right wheel forward",   20 },
-  { "RightWheelJoint", "Move right wheel backward", -20 },
-  { "ShoulderMotor",   "Move shoulder forward",      20 },
-  { "ShoulderMotor",   "Move shoulder backward",    -20 },
-  { "ElbowMotor",      "Move elbow forward",         20 },
-  { "ElbowMotor",      "Move elbow backward",       -20 },
-  { "WristMotor",      "Move wrist forward",         20 },
-  { "WristMotor",      "Move wrist backward",       -20 }
+  { "LeftWheelJoint",  "Move left wheel forward",    1 },
+  { "LeftWheelJoint",  "Move left wheel backward",  -1 },
+  { "RightWheelJoint", "Move right wheel forward",   1 },
+  { "RightWheelJoint", "Move right wheel backward", -1 },
+  { "ShoulderMotor",   "Move shoulder forward",      1 },
+  { "ShoulderMotor",   "Move shoulder backward",    -1 },
+  { "ElbowMotor",      "Move elbow forward",         1 },
+  { "ElbowMotor",      "Move elbow backward",       -1 },
+  { "WristMotor",      "Move wrist forward",         1 },
+  { "WristMotor",      "Move wrist backward",       -1 }
 };
 
 /*
